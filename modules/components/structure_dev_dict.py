8feb35fed52150076930b650178fdbd3b227dece
kanishkaHM <kanishka@happymonk.co>
2023-07-27 17:05:08 +0000

    added nvmultiribin

--- modules/components/structure_dev_dict.py
+++ modules/components/structure_dev_dict.py
@@ -5,10 +5,10 @@ import ast
 from dotenv import load_dotenv
 import shutil
 import asyncio
-import threading 
-
+import threading
 from modules.db.db_push import gst_hls_push
 from modules.db.db_fetch_devices import filter_devices
+import cv2 
 
 # cwd = os.getcwd()
 # data_path = join(cwd, 'data')
@@ -26,8 +26,10 @@ def create_device_dict():
     # load_lmdb_fst(mem_data)
     # load_lmdb_list()
 
+    
     device_det = filter_devices()
     dev_details = [""]
+
     for i,chunk in enumerate(device_det):
         device_dict = {}
         device_dict["deviceId"] = chunk[0]
@@ -40,13 +42,16 @@ def create_device_dict():
         device_dict["username"] = chunk[7]
         device_dict["rtsp"] = chunk[8]
         # device_dict["rtsp"] = rtsp_links[i]
-        # device_dict["rtsp"] = "rtsp://happymonk:admin123@streams.ckdr.co.in:4554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif"
+        #device_dict["rtsp"] = "rtsp://happymonk:admin123@streams.ckdr.co.in:6554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif"
         device_dict["password"] = chunk[9]
         device_dict["subscriptions"] = chunk[10]
         device_dict["lat"] = chunk[11]
         device_dict["long"] = chunk[12]
         dev_details.append(device_dict)
-        # if i == 3:
-        #     break
+            
+        # if i == 2:
+        #     break    
+       
+        
     threading.Thread(target=gst_hls_push,args=(dev_details,)).start()
     return dev_details
