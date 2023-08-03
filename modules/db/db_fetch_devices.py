4f847d24d3f0619c2df9aef5b1564df727761150
sriharihappymonk <srihari@happymonk.co>
2023-08-03 06:10:19 +0000

    pushing latest tenants ids

--- modules/db/db_fetch_devices.py
+++ modules/db/db_fetch_devices.py
@@ -25,19 +25,43 @@ tenant_ids = os.getenv("TENANT_IDS")
 print(pg_url, pgdb, pgport, pguser, pgpassword)
 
 def fetch_db():
-    # try:
-    # Establish a connection to the PostgreSQL database
-    connection = psycopg2.connect(host=pg_url, database=pgdb, port=pgport, user=pguser, password=pgpassword)
-    # Create a cursor object
-    cursor=connection.cursor()
-    query =  '''CREATE TABLE log02082023 PARTITION OF logs
-    FOR VALUES FROM ('2023-08-01') TO ('2023-08-02')
-        ;'''
-    cursor.execute(query)
-    connection.commit()
-        # print("Selecting rows from device table using cursor.fetchall")
-        # device_records = cursor.fetchall()
-        # return(device_records)
+    try:
+        # Establish a connection to the PostgreSQL database
+        connection = psycopg2.connect(host=pg_url, database=pgdb, port=pgport, user=pguser, password=pgpassword)
+        # Create a cursor object
+        cursor=connection.cursor()
+        query =  '''SELECT dev.id, dev."tenantId", metadev.urn, metadev.ddns, metadev.ip, CAST(metadev.port AS INTEGER), metadev."videoEncodingInformation", dev."remoteUsername", metadev.rtsp, dev."remoteDeviceSalt", ARRAY_AGG(DISTINCT feat."name") AS feature_names, ge.latitude, ge.longitude
+                FROM "Device" dev
+                INNER JOIN "DeviceMetaData" metadev ON dev."deviceId" = metadev."deviceId"
+                INNER JOIN "DeviceFeatures" devfeat ON dev."deviceId" = devfeat."deviceId" AND devfeat.enabled = True
+                INNER JOIN "Features" feat ON devfeat."featureId" = feat.id
+                INNER JOIN "Geo" ge ON ge."deviceMetaDataId" = metadev.id
+                WHERE dev.id IS NOT NULL AND dev."tenantId" IS NOT NULL AND dev."remoteUsername" IS NOT NULL AND dev."remoteDeviceSalt" IS NOT NULL AND dev.deleted = False
+                GROUP BY dev.id, dev."tenantId", metadev.urn, metadev.ddns, metadev.ip, metadev.port, metadev."videoEncodingInformation", dev."remoteUsername", metadev.rtsp, dev."remoteDeviceSalt", ge.latitude, ge.longitude;
+            ;'''
+        cursor.execute(query)
+        connection.commit()
+        print("Selecting rows from device table using cursor.fetchall")
+        device_records = cursor.fetchall()
+        return(device_records)
+    except (Exception, psycopg2.Error) as error:
+        print("Error while fetching data from PostgreSQL", error)
+        connection.rollback()  # rollback the transaction if an error occurs
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+def filter_devices():
+    dev_det = fetch_db()
+    updated_device_det =[]
+    for i in range(len(dev_det)):
+        if dev_det[i][1] in tenant_ids:
+            updated_device_det.append(dev_det[i])
+    return updated_device_det
+
 
 
 
