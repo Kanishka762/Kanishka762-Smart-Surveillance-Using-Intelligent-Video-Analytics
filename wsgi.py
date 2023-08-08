00482958352369c24f27d06c8ef52eec406409a6
nivetheni-hm <nivetheni@happymonk.co>
2023-08-08 07:29:30 +0000

    created separate script for fire model

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,19 @@
 from init import *
 from modules.components.load_paths import *
-from modules.deepstream.rtsp2frames import start_deepstream
+from modules.deepstream.person_model import start_person_model
+from modules.deepstream.fire_model import start_fire_model
 from modules.components.structure_dev_dict import create_device_dict
-
+import threading
+firesmoke = []
+basee = []
 if __name__ == '__main__':
     dev_details = create_device_dict()
-    start_deepstream(dev_details)
+    for each in dev_details:
+        if "Fire/Smoke" in each["subscriptions"]:
+            firesmoke.append(each)
+        else:
+            basee.append(each)
+
+    threading.Thread(target=start_person_model,args=(basee,)).start()
+    start_fire_model(firesmoke)
 
